8c81d251cce40022c0e255a833066472506a5f4b
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-04-04 15:22:08 +0100

    function to get balance across minimisation factors

---
+++ R/balance.R
@@ -0,0 +1,44 @@
+#' Get balance of minimisation
+#'
+#' Get balance minimisation randomisation
+#'
+#' @param object An object of class `mini`
+#'
+#' @return An object of class `balance.mini`
+#'
+#' @export
+balance <- function(object) {
+
+  factors <- object$factors
+  tab <- list()
+  for(i in 1:length(factors)) {
+    tab[[i]] <- table(object$data[,c("Group", factors[i])])
+    names(tab)[i] <- factors[i]
+  }
+
+  imbalance <- rep(NA, length(factors))
+  for(i in 1:length(factors)) {
+    imbalance[i] <- sum(apply(tab[[i]], 2, sd))
+  }
+  imbalance <- sum(imbalance)
+
+  out <- list(balance = tab, imbalance = imbalance)
+  class(out) <- "balance.mini"
+
+  return(out)
+
+}
+
+#' @export
+print.balance.mini <- function(x, ...) {
+  temp <- list()
+
+  for(i in 1:length(x$balance)) {
+    temp[[i]] <- knitr::kable(x$balance[[i]],
+                              format = "markdown")
+  }
+  cat("Balance\n")
+  cat(rep("-", 80), "\n", sep = "")
+  cat(do.call("paste", c(temp, sep = "    ")), sep = "\n")
+  cat("\nTotal imbalance:", round(x$imbalance, 3))
+}
