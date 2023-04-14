c2c1de74d7edde9235280490700529a3303635f8
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-04-14 09:03:57 +0100

    include strata in balance summaries

--- R/balance.R
+++ R/balance.R
@@ -9,23 +9,32 @@
 #' @export
 balance <- function(object) {
 
+  # Balance across factors
   factors <- factors(object)
-  tab <- list()
+  f_tab <- list()
   for(i in 1:length(factors)) {
-    tab[[i]] <- table(object[,c("Group", factors[i])])
-    names(tab)[i] <- factors[i]
+    f_tab[[i]] <- table(object[,c("Group", factors[i])])
+    names(f_tab)[i] <- factors[i]
   }
 
   imbalance <- rep(NA, length(factors))
   for(i in 1:length(factors)) {
-    temp <- t(tab[[i]]) %*% diag(1/ratio(object))
+    temp <- t(f_tab[[i]]) %*% diag(1/ratio(object))
     imbalance[i] <- sum(apply(temp, 1, stats::sd))
   }
 
   imbalance <- sum(imbalance)
 
-  out <- list(balance = tab, imbalance = imbalance, groups = groups(object),
-              factors = factors, ratio = ratio(object))
+  out <- list(factor_balance = f_tab, imbalance = imbalance,
+              groups = groups(object), factors = factors, ratio = ratio(object))
+
+  # Balance across strata
+  if(!is.null(strata(object))) {
+    s_tab <- table(object[,c("Group", strata(object))])
+    out <- c(out, strata_balance = list(s_tab))
+    strata(out) <- strata(object)
+  }
+
   class(out) <- "balance.mini"
 
   return(out)
@@ -34,15 +43,24 @@ balance <- function(object) {
 
 #' @export
 print.balance.mini <- function(x, ...) {
-  temp <- list()
 
-  for(i in 1:length(x$balance)) {
-    temp[[i]] <- knitr::kable(x$balance[[i]],
-                              format = "markdown")
+  f_temp <- list()
+  for(i in 1:length(x$factor_balance)) {
+    f_temp[[i]] <- knitr::kable(x$factor_balance[[i]], format = "markdown")
   }
-  cat("Balance of factors (", paste(x$factors, collapse = ", "), ") over",
-      x$groups, "groups (", paste(x$ratio, collapse = ":"), ")\n")
+
+  if(!is.null(strata(x))) {
+    s_temp <- knitr::kable(x$strata_balance, format = "markdown")
+  }
+
+  cat("Balance of factors (", paste(x$factors, collapse = ", "), ") over ",
+      x$groups, " groups (", paste(x$ratio, collapse = ":"), ")\n", sep = "")
   cat(rep("-", 80), "\n", sep = "")
-  cat(do.call("paste", c(temp, sep = "    ")), sep = "\n")
+  cat(do.call("paste", c(f_temp, sep = "    ")), sep = "\n")
+  if(!is.null(strata(x))) {
+    cat("\nBalance of strata (", strata(x), ")\n", sep = "")
+    cat(rep("-", 80), "\n", sep = "")
+    cat(s_temp, sep = "\n")
+  }
   cat("\nTotal imbalance:", round(x$imbalance, 3))
 }
