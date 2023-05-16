1da8e5f96be34649284c28930f27e84c18a74758
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-05-16 15:25:48 +0100

    adding system data to facilitate tests

---
+++ data-raw/patients.R
@@ -0,0 +1,7 @@
+## code to prepare `patients` dataset goes here
+patients <- data.frame(sex = sample(c("M", "F"), 150, replace = T),
+                       stage = sample(c("I", "II", "III"), 150, replace = T,
+                                      prob = c(0.5, 0.3, 0.2)),
+                       site = sample(1:10, 150, replace = T))
+
+usethis::use_data(patients, internal = TRUE, overwrite = TRUE)
