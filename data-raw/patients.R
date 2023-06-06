7988af94de02aadf4f32326681dc78e10f2473c0
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-06-06 17:01:02 +0100

    patients data no longer internal

--- data-raw/patients.R
+++ data-raw/patients.R
@@ -4,4 +4,4 @@ patients <- data.frame(sex = sample(c("M", "F"), 150, replace = T),
                                       prob = c(0.5, 0.3, 0.2)),
                        site = sample(1:10, 150, replace = T))
 
-usethis::use_data(patients, internal = TRUE, overwrite = TRUE)
+usethis::use_data(patients, overwrite = TRUE)
