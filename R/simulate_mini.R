e51a448712c655ee4cc9cfc31e7e33d621ab1f74
EstherHerbert <e.herbert@sheffield.ac.uk>
2024-01-26 10:01:42 +0000

    simulate data when simulating minimisation

--- R/simulate_mini.R
+++ R/simulate_mini.R
@@ -1,17 +1,20 @@
 #' Simulation of minimisation
 #'
-#' `simulate_mini` repeatedly minimises a data set of patients with different
-#' specified burnin periods and/or minimisation probabilities. Its purpose is to
-#' guide decisions when designing a randomisation strategy for a randomised
-#' controlled trial.
+#' `simulate_mini` repeatedly generates random data and then minimises with
+#'   different specified burnin periods and/or minimisation probabilities. Its
+#'   purpose is to guide decisions when designing a randomisation strategy for a
+#'   randomised controlled trial.
 #'
-#' @param data a `data.frame` object with one line per participant and columns
-#'             for the minimisation factors.
+#' @param sampsize the desired sample size of the data, i.e., the sample size
+#'   of your prospective trial.
+#' @param factors a list of factors, each a list containing two items. The first
+#'   is `levels` which is either a vector of the level names OR the number of
+#'   levels. The second item is either `props`, a vector of proportions equal to
+#'   the number of levels in the factor; OR prop.dist which is a named vector
+#'   containing the mean and sd of the proportions.
 #' @param Nsims an integer, the number of simulations to run per scenario,
 #'              default is 100.
 #' @param groups an integer, the number of groups to randomise, default is 3.
-#' @param factors a list of character vectors with the names of the factors for
-#'                minimisation
 #' @param burnin a vector of integers of possible burnin lengths before
 #'               minimisation kicks in. Individual values must be > 0 and
 #'               < total sample size.
@@ -21,46 +24,33 @@
 #'              equal to the number of groups).
 #'
 #' @export
-simulate_mini <- function(data, Nsims = 100, groups = 3, factors, burnin, minprob,
-                          ratio = rep(1, groups)) {
-
-  if(any(!unlist(factors) %in% names(data))) {
-    stop("Given factors must be variables in the data.")
-  }
+simulate_mini <- function(sampsize, factors, Nsims = 100, groups = 3, burnin,
+                          minprob, ratio = rep(1, groups)) {
 
   inputs <- expand.grid(sim.no = 1:Nsims,
-                        factors = factors,
                         burnin = burnin,
                         minprob = minprob)
 
-  minimise_s <- purrr::possibly(minimise, otherwise = NA)
-
   sims <- mapply(
-    function(x, y, z, w) minimise_s(data, factors = z, burnin = x,
-                                    minprob = y, ratio = ratio),
-    inputs$burnin, inputs$minprob, inputs$factors,
+    function(x, y) {
+      data <- simulate_data(sampsize, factors)
+      minimise(data = data, groups = groups, factors = names(factors),
+               burnin = x, minprob = y, ratio = ratio)
+    },
+    inputs$burnin, inputs$minprob,
     SIMPLIFY = FALSE
   )
 
-  if(any(is.na(sims))) {
-    warning(paste("Some combinations of inputs could not be used for",
-                  "minimisation, they have been removed from the list of",
-                  "inputs."), call. = FALSE)
-  }
-
-  inputs$minprob <- sapply(inputs$minprob, paste, collapse = ", ")
-  inputs$factors <- sapply(inputs$factors, paste, collapse = ", ")
-
-  inputs <- inputs[!is.na(sims),]
-  sims <- sims[!is.na(sims)]
-
   imbalance <- mapply(function(x) balance(x)$imbalance, sims)
   group.sizes <- t(sapply(sims, function(x) with(x, table(Group))))
+
+  inputs$minprob <- sapply(inputs$minprob, paste, collapse = ", ")
 
   out <- list(inputs = inputs, simulations = sims, group.sizes = group.sizes,
               imbalance = imbalance)
 
   class(out) <- "mini.sim"
+  factors(out) <- names(factors)
 
   return(out)
 
@@ -73,16 +63,16 @@ print.mini.sim <- function(x, ...) {
   temp$imbalance <- x$imbalance
   temp$groups <- x$group.sizes
 
-  tab_imb <- stats::aggregate(imbalance ~ factors + burnin + minprob ,
+  tab_imb <- stats::aggregate(imbalance ~ burnin + minprob ,
                               data=temp, FUN = function(x) round(mean(x), 1))
 
-  tab_grp <- stats::aggregate(groups ~ factors + burnin + minprob,
+  tab_grp <- stats::aggregate(groups ~ burnin + minprob,
                               data=temp, FUN = function(x) round(mean(x), 1))
 
   cat("Simulation of Multi-arm Minimisation \n")
   cat(rep("-", 80), "\n", sep = "")
   cat("Number of simulations per scenario:", max(x$inputs$sim.no), "\n")
-  cat("Factor options:", paste(unique(x$inputs$factor), collapse = "; "), "\n")
+  cat("Factors:", paste(factors(x), collapse = ", "), "\n")
   cat("Burnin options:", paste(unique(x$inputs$burnin), collapse = ", "), "\n")
   cat("Minimisation probability options:",
       paste(unique(x$inputs$minprob), ncollapse = "; "), "\n")
