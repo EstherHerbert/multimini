8592de81fc52f53ec1ecb69c102e87331a055d0e
EstherHerbert <e.herbert@sheffield.ac.uk>
2025-02-06 15:34:33 +0000

    new column in patient data for eligibility

--- R/data.R
+++ R/data.R
@@ -3,10 +3,11 @@
 #' A randomly generated data set of "patient" data
 #'
 #' @format ## `patients`
-#' A data frame with 150 rows and 3 columns:
+#' A data frame with 150 rows and 4 columns:
 #' \describe{
 #'    \item{sex}{Patient sex (M or F)}
 #'    \item{stage}{Stage of disease (I, II, or III)}
 #'    \item{site}{Recruitment site (1 to 10)}
+#'    \item{eligible}{Which treatments are they eligible for? (A, B, C)}
 #' }
 "patients"
--- data-raw/patients.R
+++ data-raw/patients.R
@@ -2,6 +2,8 @@
 patients <- data.frame(sex = sample(c("M", "F"), 150, replace = T),
                        stage = sample(c("I", "II", "III"), 150, replace = T,
                                       prob = c(0.5, 0.3, 0.2)),
-                       site = sample(1:10, 150, replace = T))
+                       site = sample(1:10, 150, replace = T),
+                       eligible = sample(c("ABC", "AB", "AC", "BC"), 150,
+                                         replace = T))
 
 usethis::use_data(patients, overwrite = TRUE)
