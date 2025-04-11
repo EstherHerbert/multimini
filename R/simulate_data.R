d2431f6fc8a0708b6cb194a7c868b6ae6fec524e
EstherHerbert <e.herbert@sheffield.ac.uk>
2025-04-11 11:22:48 +0100

    add eligibility to simulations of data

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -10,6 +10,10 @@
 #'   levels. The second item is either `props`, a vector of proportions equal to
 #'   the number of levels in the factor; OR prop.dist which is a named vector
 #'   containing the mean and sd of the proportions.
+#' @param eligibility logical, should the data include group eligibility.
+#'   If `TRUE` then must supply `groups` argument.
+#' @param groups either a character vector of group names or an integer
+#'   specifying the number of groups.
 #'
 #' @examples
 #' simulate_data(factors = list(sex = list(levels = c("M", "F"),
@@ -18,12 +22,35 @@
 #'                                          prop.dist = c(mean = 0.1, sd = 0.05))),
 #'               sampsize = 150)
 #'
+#' simulate_data(factors = list(sex = list(levels = c("M", "F"),
+#'                                         props = c(0.5, 0.5)),
+#'                              site = list(levels = 12,
+#'                                          prop.dist = c(mean = 0.1, sd = 0.05))),
+#'               sampsize = 100, eligibility = TRUE, groups = 4)
+#'
 #' @export
-simulate_data <- function(sampsize, factors) {
+simulate_data <- function(sampsize, factors, eligibility = FALSE, groups = NULL) {
+
+  if(eligibility & is.null(groups)) {
+    stop("Must supply `groups` if `eligibility = TRUE`")
+  }
+
+  if (eligibility) {
+    if (length(groups) == 1 & rlang::is_integerish(groups)) {
+      groups <- LETTERS[1:groups]
+    }
+    temp <- list()
+    for(i in 2:length(groups)) {
+      temp <- c(temp, utils::combn(groups, i, simplify = F))
+    }
+    groups <- sapply(temp, paste, collapse = "")
+  }
 
   data <- lapply(factors, gen_factor, sampsize = sampsize)
 
   data <- data.frame(ID = 1:sampsize, data)
+
+  data$eligible <- sample(groups, sampsize, replace = T)
 
   return(data)
 
