6227137919785200232910ef37c881a68b9bf060
EstherHerbert <e.herbert@sheffield.ac.uk>
2025-04-15 11:10:13 +0100

    added more tests

--- tests/testthat/test-minimise.R
+++ tests/testthat/test-minimise.R
@@ -1,5 +1,12 @@
-test_that("errors given", {
+test_that("errors given for wrong inputs", {
   expect_error(minimise(patients, groups = 1, factors = c("sex", "site")))
+  expect_error(minimise(patients, groups = 2, factors = "sex", minprob = 1.1))
+  expect_error(minimise(patients, groups = 2, factors = c("sex", "age")))
+  expect_error(minimise(patients, groups = 2, factors = "stage",
+                        ratio = c(1,2,1)))
+  expect_error(minimise(patients, factors = "sex", burnin = 200))
+  temp <- within(patients, rm(eligible))
+  expect_error(minimise(temp, factors = "sex", check.eligibility = T))
 })
 
 test_that("seed works", {
@@ -13,4 +20,14 @@ test_that("seed works", {
       minimise(patients, groups = 2, factors = c("sex"), burnin = 5)
     )
   )
+})
+
+test_that("eligibility correct", {
+  expect_true({
+    minimise(patients, factors = "sex", check.eligibility = T) %>%
+      as.data.frame() %>%
+      with(purrr::map2(Group, eligible, ~.x %in%.y)) %>%
+      unlist() %>%
+      all()
+  })
 })
